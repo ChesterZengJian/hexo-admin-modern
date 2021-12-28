060ce985ab8940d45c06ce3155e334b960fe116a
Chester <374488168@qq.com>
2021-12-29 01:19:49 +0800

    add authentication function

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ var serveStatic = require('serve-static'),
   path = require('path'),
   api = require('./src/services/api');
 
-// var passwordProtected = hexo.config.admin && hexo.config.admin.username;
+var passwordProtected = hexo.config.admin && hexo.config.admin.username;
 
 // verify that correct config options are set.
-// if (passwordProtected) {
-//   if (!hexo.config.admin.password_hash) {
-//     console.error('[Hexo Admin]: config admin.password_hash is requred for authentication');
-//     passwordProtected = false;
-//   } else if (hexo.config.admin.password_hash.length <= 32) {
-//     throw new Error('[Hexo Admin]: the provided password_hash looks like an md5 hash -- hexo-admin has switched to use bcrypt; see the Readme for more info.')
-//   }
+if (passwordProtected) {
+  if (!hexo.config.admin.password_hash) {
+    console.error('[Hexo Admin]: config admin.password_hash is requred for authentication');
+    passwordProtected = false;
+  } else if (hexo.config.admin.password_hash.length <= 32) {
+    throw new Error('[Hexo Admin]: the provided password_hash looks like an md5 hash -- hexo-admin has switched to use bcrypt; see the Readme for more info.')
+  }
 
-//   if (!hexo.config.admin.secret) {
-//     console.error('[Hexo Admin]: config admin.secret is requred for authentication');
-//     passwordProtected = false;
-//   }
+  if (!hexo.config.admin.secret) {
+    console.error('[Hexo Admin]: config admin.secret is requred for authentication');
+    passwordProtected = false;
+  }
 
-// }
+}
 
 hexo.extend.filter.register('server_middleware', function (app) {
   console.log("hexo-admin-modern")
 
-  // if (passwordProtected) {
-  //   require('./auth')(app, hexo);   // setup authentication, login page, etc.
-  // }
+  if (passwordProtected) {
+    console.log("password=", passwordProtected)
+    require('./src/auth')(app, hexo);   // setup authentication, login page, etc.
+  }
 
   // Main routes
   app.use(hexo.config.root + 'admin/', serveStatic(path.join(__dirname, 'build')));

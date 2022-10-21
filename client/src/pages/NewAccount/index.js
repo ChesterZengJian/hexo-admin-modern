d7a8326a4e9a3e99065d9dcce07c61d413139ef9
Chester <374488168@qq.com>
2022-10-22 02:07:47 +0800

    add new account function

--- client/src/pages/NewAccount/index.js
+++ client/src/pages/NewAccount/index.js
@@ -1,10 +1,15 @@
-import { Card, Form, Input, Button, message } from 'antd';
+import { Card, Form, Input, Button, message, Result, Typography } from 'antd';
+import { useState } from 'react';
+import { http } from '@/utils';
 import logo from '@/assets/logo.svg';
 import './index.scss';
-import { http } from '@/utils';
 
 function NewAccount() {
-    async function onFinish(values) {
+    const { Paragraph } = Typography;
+    const [showResult, setShowResult] = useState(false);
+    const [account, setAccount] = useState({});
+
+    const onFinish = async (values) => {
         const { username, password, secret } = values;
         try {
             const res = await http.post('/admin/api/accounts', {
@@ -12,6 +17,7 @@ function NewAccount() {
                 password,
                 secret,
             });
+            setAccount(res.data);
             console.log('res:', res.data);
         } catch (error) {
             message.error('生成账号失败');
@@ -19,66 +25,101 @@ function NewAccount() {
         }
 
         // 生成账号弹窗
-        // const success = () => {
-        //     Modal.success({
-        //         content: 'some messages...some messages...',
-        //     });
-        // };
-    }
+        setShowResult(true);
+    };
 
     return (
         <div className="login">
-            <Card className="login-container">
-                <img className="login-logo" src={logo} alt="logo" />
-                <Form
-                    validateTrigger={['onBlur', 'onChange']}
-                    onFinish={onFinish}
-                >
-                    <Form.Item
-                        name="username"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入用户名',
-                            },
-                        ]}
+            {!showResult ? (
+                <Card className="login-container">
+                    <img className="login-logo" src={logo} alt="logo" />
+                    <Form
+                        validateTrigger={['onBlur', 'onChange']}
+                        onFinish={onFinish}
                     >
-                        <Input size="large" placeholder="请输入用户名" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入密码',
-                            },
-                        ]}
-                    >
-                        <Input size="large" placeholder="请输入密码" />
-                    </Form.Item>
-                    <Form.Item
-                        name="secret"
-                        rules={[
-                            {
-                                required: true,
-                                message: '请输入加密密钥',
-                            },
-                        ]}
-                    >
-                        <Input size="large" placeholder="请输入加密密钥" />
-                    </Form.Item>
-                    <Form.Item>
-                        <Button
-                            type="primary"
-                            htmlType="submit"
-                            size="large"
-                            block
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入用户名',
+                                },
+                            ]}
                         >
-                            生成账号密码
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
+                            <Input size="large" placeholder="请输入用户名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入密码',
+                                },
+                            ]}
+                        >
+                            <Input size="large" placeholder="请输入密码" />
+                        </Form.Item>
+                        <Form.Item
+                            name="secret"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入加密密钥',
+                                },
+                            ]}
+                        >
+                            <Input size="large" placeholder="请输入加密密钥" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                size="large"
+                                block
+                            >
+                                生成账号密码
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            ) : (
+                <Card className="result-container">
+                    <Result
+                        status="success"
+                        title="成功创建账号"
+                        subTitle={
+                            <Typography>
+                                <Paragraph>
+                                    复制下面代码到 _config.yml 并重启 HEXO.
+                                    <pre className="result-code">
+                                        <code>
+                                            # hexo-admin authentification <br />
+                                            admin: <br />
+                                            &nbsp;&nbsp;username:
+                                            {account.username} <br />
+                                            &nbsp;&nbsp;password_hash:
+                                            {account.password}
+                                            <br />
+                                            &nbsp;&nbsp;secret: {account.secret}
+                                        </code>
+                                    </pre>
+                                </Paragraph>
+                            </Typography>
+                        }
+                        extra={[
+                            <Button
+                                type="primary"
+                                key="console"
+                                onClick={() => {
+                                    setShowResult(false);
+                                }}
+                            >
+                                重新创建账号
+                            </Button>,
+                        ]}
+                    />
+                </Card>
+            )}
         </div>
     );
 }

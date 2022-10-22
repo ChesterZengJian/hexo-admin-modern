0c1abbd769f64c438ae443904f7409b8b0b2ebe6
Chester <374488168@qq.com>
2022-10-22 22:02:25 +0800

    fixed problem that 401 request cannot redirect to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,67 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import DefaultLayout from '@/pages/Layout';
-import './App.scss';
+import { lazy, Suspense } from 'react';
+import {
+    Routes,
+    Route,
+    unstable_HistoryRouter as HistoryRouter,
+} from 'react-router-dom';
 import { PostList, PostEdit } from './pages/Post';
-import Login from './pages/Login';
 import { RoutingGuard } from '@/components/RoutingGuard.js';
-import NewAccount from './pages/NewAccount';
+import { history } from '@/utils';
+// import DefaultLayout from '@/pages/Layout';
+// import Login from './pages/Login';
+// import NewAccount from './pages/NewAccount';
+
+import './App.scss';
+
+const Login = lazy(() => import('./pages/Login'));
+const DefaultLayout = lazy(() => import('./pages/Layout'));
+const NewAccount = lazy(() => import('./pages/NewAccount'));
 
 function App() {
     return (
-        <HashRouter>
+        <HistoryRouter history={history}>
             <div className="app">
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <RoutingGuard>
-                                <DefaultLayout />
-                            </RoutingGuard>
-                        }
-                    >
-                        <Route index path="/" element={<PostList />}></Route>
-                        <Route path="/posts/new" element={<PostEdit />}></Route>
-                        <Route path="/posts/:id" element={<PostEdit />}></Route>
-                    </Route>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route
-                        path="/accounts/new"
-                        element={<NewAccount />}
-                    ></Route>
-                </Routes>
+                <Suspense
+                    fallback={
+                        <div style={{ textAlign: 'center', marginTop: 200 }}>
+                            Loading.....
+                        </div>
+                    }
+                >
+                    {/* <HashRouter> */}
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <RoutingGuard>
+                                    <DefaultLayout />
+                                </RoutingGuard>
+                            }
+                        >
+                            <Route
+                                index
+                                path="/"
+                                element={<PostList />}
+                            ></Route>
+                            <Route
+                                path="/posts/new"
+                                element={<PostEdit />}
+                            ></Route>
+                            <Route
+                                path="/posts/:id"
+                                element={<PostEdit />}
+                            ></Route>
+                        </Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route
+                            path="/accounts/new"
+                            element={<NewAccount />}
+                        ></Route>
+                    </Routes>
+                </Suspense>
+                {/* </HashRouter> */}
             </div>
-        </HashRouter>
+        </HistoryRouter>
     );
 }
 
